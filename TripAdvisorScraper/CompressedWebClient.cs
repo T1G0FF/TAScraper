6ce8b07f3bc4c8993530c13cf6bad54f40f880a1
Thomas <thomas.gitcommit@outlook.com>
2015-07-25 14:26:12 +1000

    Small web client that decompresses requests automatically.

---
+++ TripAdvisorScraper/CompressedWebClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace TripAdvisorScraper
+{
+    public class CompressedWebClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            HttpWebRequest request = (HttpWebRequest)base.GetWebRequest(address);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            return request;
+        }
+    }
+}
